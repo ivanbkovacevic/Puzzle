2851815267b58f142a48a7504e4b66e47bd9953b
ivanbkovacevic <ivanbkovacevic@gmail.com>
2021-04-26 18:02:20 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import './App.scss';
+import './App.css';
 import Container from './components/Container'
 import { CropImage} from './components/Utility'
 import OriginalImage from './components/OriginalImage'
-import startImage from './birds.jpg'
+import startImage from './kerber_character.png'
 
 class App extends Component {
 state={
@@ -12,35 +12,38 @@ state={
 }
 
 
-onSelectFile = (e) => {
-  if (e.target.files && e.target.files.length > 0) {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => {
-      this.setState({ myPicture: reader.result });
-    });
-    reader.readAsDataURL(e.target.files[0]);
-  }
-}
+// onSelectFile = (e) => {
+//   if (e.target.files && e.target.files.length > 0) {
+//     const reader = new FileReader();
+//     reader.addEventListener('load', () => {
+//       this.setState({ myPicture: reader.result });
+//     });
+//     reader.readAsDataURL(e.target.files[0]);
+//   }
+// }
 
-onSelectURL=(e)=>{
-  console.log(e.target.value)
-  this.setState({myPicture:e.target.value});
-}
+// onSelectURL=(e)=>{
+//   console.log(e.target.value)
+//   this.setState({myPicture:e.target.value});
+// }
 
   render() {
    
     return (
-      <div className="main-container">
-             <div className="my-form">
+      <div className="wraper">
+   <div className="main-container">
+             {/* <div className="my-form">
              <input type="file" onChange={this.onSelectFile} />
            
               <input onChange={this.onSelectURL} type="text"  
                         placeholder="OR paste the image URL address" size="30" name="myPicture"></input>
-              </div>
+              </div> */}
                 <Container myPicture={this.state.myPicture}/>   
-               <OriginalImage myPicture={this.state.myPicture}/>    
+               {/* <OriginalImage myPicture={this.state.myPicture}/>     */}
               
       </div>
+      </div>
+   
     );
   }
 }
